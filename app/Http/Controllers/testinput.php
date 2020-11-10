6aa16326c08a6af744814b5683420a72934d1d45
arya_iw <aryairama987@gmail.com>
2020-11-10 18:32:25 +0700

    first commit

---
+++ app/Http/Controllers/testinput.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class testinput extends Controller
+{
+    public function tambahinput(Request $request)
+    {
+        $jam = array();
+        $arr = array();
+        $mapel = array();
+        $hasiltotal = 0;
+        $input = $request->kolom;
+        $nilai = array();
+        if ($request->kirim) {
+            for ($i = 1 ;$i<= $input;$i++) {
+                if ($request->input('nilai'.$i) == "A") {
+                    $arr[$i] = 100;
+                } elseif ($request->input('nilai'.$i) == "B") {
+                    $arr[$i] = 80;
+                } elseif ($request->input('nilai'.$i) == "C") {
+                    $arr[$i] = 60;
+                } elseif ($request->input('nilai'.$i) == "D") {
+                    $arr[$i] = 40;
+                } elseif ($request->input('nilai'.$i) == "E") {
+                    $arr[$i] = 20;
+                } elseif ($request->input('nilai'.$i) == "F") {
+                    $arr[$i] = 0;
+                }
+                $nilai[$i] = $request->input('nilai'.$i);
+                $jam[$i] = $request->input('jam'.$i);
+                $mapel[$i] = $request->input('pelajaran'.$i);
+            }
+            return view('testinput', \compact('arr', 'input', 'request','nilai','jam','mapel'));
+        }
+        return view('testinput', \compact('input', 'hasiltotal'));
+    }
+}
---
+++ resources/views/testinput.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="{{route('test.test')}}" method="get">
+        <label for="">Masukkan Jumlah Input</label><input type="text" name="kolom" value="{{ $input }}">
+        <button type="submit">klik</button>
+        <br>
+        <br>
+        <label for="">Jumlah Kolom : {{ $input }}</label>
+        @if ($input)
+        @for ($i = 1; $i <= $input; $i++) <br><label for="">Mata Pelajaran {{ $i }}</label>
+            <input type='test' name="pelajaran{{ $i }}">
+            <label for="">Jam</label>
+            <input type="text" name="jam{{ $i }}">
+            <label for="">Nilai</label>
+            <input type="text" name="nilai{{ $i }}">
+            @endfor
+            @endif
+            @if ($input)
+            <input type="submit" name="kirim" value="kirim">
+            @endif
+            @if ($arr ?? '')
+            <table border="1">
+                <thead>
+                    <tr>
+                        <th>Mapel</th>
+                        <th>Jam</th>
+                        <th>Nilai</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        @for ($i = 0; $i < $input; $i++)
+                            <tr>
+                                <td>{{ $mapel[$i+1] }}</td>
+                                <td>{{ $jam[$i+1] }}</td>
+                                <td>{{ $nilai[$i+1] }}</td>
+                            </tr>
+                        @endfor
+                        <tr>
+                            <td colspan="{{ $input }}">
+                                @php
+                                $total = array_sum($arr) / $input;
+                                print_r($total);
+                                @endphp</td>
+                        </tr>
+                    </tr>
+                </tbody>
+            </table>
+            @endif
+    </form>
+</body>
+
+</html>
